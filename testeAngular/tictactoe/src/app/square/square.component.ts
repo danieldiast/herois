fdb0d6259ac70d36e159b684fbe5e9857be1a0be
Daniel Dias <danieldiast@gmail.com>
2020-05-12 02:41:27 -0300

    adicionado testeang tictactoe

---
+++ testeAngular/tictactoe/src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent {
+  rando = Math.random();
+
+  @Input() value: 'X' | 'O';
+
+  // constructor(){
+  //   // setInterval(() => this.rando = Math.random(), 4000);
+
+  // }
+}
